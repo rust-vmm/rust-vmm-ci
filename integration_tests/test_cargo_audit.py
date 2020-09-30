a0a6d265e64f4da101f4003bf2342bb918c3f113
Anthony Riley II <anth1y@github.com>
2020-09-30 07:37:54 -0700

    We no longer need this

--- integration_tests/test_cargo_audit.py
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-import subprocess 
-
-def test_cargo_audit():
-    cmd = 'cargo-audit'
-    prs = subprocess.run("{}".format(cmd), shell=True, check=True)
-    stdout, nothing = prs.communicate()
