d72aecbe59e284b42f165601de172211ceb6f69c
Catalin Dumitru <catdum@amazon.com>
2021-07-20 17:06:13 +0300

    Change format checking and add version setters
    Added a check_step() function instead of the previous check_format(),
    separated the overriding of the default configuration when some env
    vars are set and added a function to set docker versions.
    
    Signed-off-by: Catalin Dumitru <catdum@amazon.com>

--- .buildkite/generate_pipeline.py
+++ .buildkite/generate_pipeline.py
@@ -7,62 +7,80 @@ import os
 CONTAINER_VERSION = "v12"
 DOCKER_PLUGIN_VERSION = "v3.8.0"
 
+X86_AGENT_TAGS = os.getenv('X86_LINUX_AGENT_TAGS')
+AARCH64_AGENT_TAGS = os.getenv('AARCH64_LINUX_AGENT_TAGS')
+DOCKER_PLUGIN_CONFIG = os.getenv('DOCKER_PLUGIN_CONFIG')
+
+PLATFORMS = ['x86_64.metal', 'arm.metal']
+OSS = ['linux']
+
 ROOT_PATH = pathlib.Path(__file__).parent.resolve()
 
 
-def check_format(json_cfg):
-    platforms = {'x86_64.metal', 'arm.metal'}
-    oss = {'linux'}
+def check_step(step):
+    assert step.get('label'),\
+        "Label missing."
+    assert step.get('commands'),\
+        "Commands missing."
+    agents = step.get('agents')
+    assert agents,\
+        "Agents missing."
+    assert agents.get('platform') in PLATFORMS,\
+        "Invalid or missing platform. The supported platforms are " + \
+        ", ".join(PLATFORMS)
+    assert agents.get('os') in OSS,\
+        "Invalid or missing os. The supported oss are " + \
+        ", ".join(OSS)
+    assert step.get('plugins'),\
+        "Plugins missing."
 
-    steps = json_cfg.get('steps')
-    assert steps,\
-        "Steps missing."
-    for step in steps:
-        assert step.get('label'),\
-            "Label missing."
-        assert step.get('commands'),\
-            "Commands missing."
-        agents = step.get('agents')
-        assert agents,\
-            "Agents missing."
-        assert agents.get('platform') in platforms,\
-            "Invalid or missing platform."
-        assert agents.get('os') in oss,\
-            "Invalid or missing os."
+
+def override_default_config(step):
+    agents = step['agents']
+    platform = agents['platform']
+
+    if X86_AGENT_TAGS and platform == 'x86_64.metal':
+        agents.clear()
+        tags = X86_AGENT_TAGS.split("; ")
+        for tag in tags:
+            key, val = tag.split(": ")
+            agents[key] = val
+
+    if AARCH64_AGENT_TAGS and platform == 'arm.metal':
+        agents.clear()
+        tags = AARCH64_AGENT_TAGS.split("; ")
+        for tag in tags:
+            key, val = tag.split(": ")
+            agents[key] = val
+
+    if DOCKER_PLUGIN_CONFIG:
+        plugins = step['plugins']
+        docker = plugins[0][f"docker#{DOCKER_PLUGIN_VERSION}"]
+        tags = DOCKER_PLUGIN_CONFIG.split("; ")
+        for tag in tags:
+            key, val = tag.split(": ")
+            val = json.loads(val)
+            docker[key] = val
+
+
+def set_versions(step):
+    plugins = step['plugins']
+    plugin = plugins[0]
+    docker = plugin['docker#{DOCKER_PLUGIN_VERSION}']
+    del plugin['docker#{DOCKER_PLUGIN_VERSION}']
+    plugin[f"docker#{DOCKER_PLUGIN_VERSION}"] = docker
+    docker['image'] = docker['image'].replace(
+        "{CONTAINER_VERSION}", CONTAINER_VERSION)
 
 
 def customize_cfg(json_cfg):
-    x86_agent_tags = os.getenv('X86_LINUX_AGENT_TAGS')
-    aarch64_agent_tags = os.getenv('AARCH64_LINUX_AGENT_TAGS')
-    docker_plugin_config = os.getenv('DOCKER_PLUGIN_CONFIG')
+    steps = json_cfg.get('steps')
+    assert steps, "Steps missing."
 
-    if x86_agent_tags or aarch64_agent_tags or docker_plugin_config:
-        steps = json_cfg['steps']
-        for step in steps:
-            agents = step['agents']
-            platform = agents['platform']
-            if x86_agent_tags and platform == 'x86_64.metal':
-                agents.clear()
-                tags = x86_agent_tags.split(", ")
-                for tag in tags:
-                    key, val = tag.split(": ")
-                    agents[key] = val
-
-            if aarch64_agent_tags and platform == 'arm.metal':
-                agents.clear()
-                tags = aarch64_agent_tags.split(", ")
-                for tag in tags:
-                    key, val = tag.split(": ")
-                    agents[key] = val
-
-            if docker_plugin_config:
-                plugins = step['plugins']
-                docker = plugins[0][f"docker#{DOCKER_PLUGIN_VERSION}"]
-                tags = docker_plugin_config.split(", ")
-                for tag in tags:
-                    key, val = tag.split(": ")
-                    val = json.loads(val)
-                    docker[key] = val
+    for step in steps:
+        check_step(step)
+        set_versions(step)
+        override_default_config(step)
 
 
 def generate_test_pipeline(json_cfg_file=f"{ROOT_PATH}/config.json"):
@@ -70,7 +88,6 @@ def generate_test_pipeline(json_cfg_file=f"{ROOT_PATH}/config.json"):
         json_cfg = json.load(json_file)
         json_file.close()
 
-    check_format(json_cfg)
     customize_cfg(json_cfg)
     yaml.dump(json_cfg, sys.stdout, sort_keys=False)
 
