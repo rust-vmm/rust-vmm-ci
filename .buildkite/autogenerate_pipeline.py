4f204cae063529c030e3331cbd9284122a9257ab
Catalin Dumitru <catdum@amazon.com>
2021-08-25 17:46:52 +0300

    Leftovers from #79
    Added copyright info and fixed typos.
    
    Signed-off-by: Catalin Dumitru <catdum@amazon.com>

--- .buildkite/autogenerate_pipeline.py
+++ .buildkite/autogenerate_pipeline.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 """
 This script is printing the Buildkite pipeline.yml to stdout.
 This can also be used as a library to print the steps from a different pipeline
--- test_run.py
+++ test_run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 import json
 import subprocess
 import platform
