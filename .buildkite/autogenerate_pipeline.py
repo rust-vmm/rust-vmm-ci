d216a46879096a4f1e83af3a7b6c90c26e1a688d
Sergii Glushchenko <gsserge@amazon.com>
2022-01-21 14:08:23 +0100

    Update to rustvmm/dev:v15 with Rust 1.58.1
    Signed-off-by: Sergii Glushchenko <gsserge@amazon.com>

--- .buildkite/autogenerate_pipeline.py
+++ .buildkite/autogenerate_pipeline.py
@@ -60,7 +60,7 @@ from textwrap import dedent
 
 # This represents the version of the rust-vmm-container used
 # for running the tests.
-CONTAINER_VERSION = "v14"
+CONTAINER_VERSION = "v15"
 # This represents the version of the Buildkite Docker plugin.
 DOCKER_PLUGIN_VERSION = "v3.8.0"
 
