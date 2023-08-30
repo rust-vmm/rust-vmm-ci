42bc0b57f2c74db559f3bbb7793f6b7b5fe16a26
Patrick Roy <roypat@amazon.co.uk>
2023-08-30 15:58:11 +0100

    Add Rust unittest so that test_coverage properly self-tests
    Using kcov, a coverage of 33.3% was reported, even though that are no
    rust-test code in the dummy crate that is included in this repository.
    This commit adds an actual unit test, so that the test_coverage test
    actually properly self-tests here.
    
    Signed-off-by: Patrick Roy <roypat@amazon.co.uk>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-pub fn main() {
-    println!("It works!");
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_add() {
+        assert_eq!(super::add(1, 2), 3)
+    }
 }
