df03b93f57dc35644e8e3c7d0d09337bafaa57ad
Wenyu Huang <huangwenyuu@outlook.com>
2025-07-28 04:09:48 +0800

    Update container version to g260df6e with lsof installed
    Signed-off-by: Wenyu Huang <huangwenyuu@outlook.com>

--- .buildkite/autogenerate_pipeline.py
+++ .buildkite/autogenerate_pipeline.py
@@ -60,7 +60,7 @@ from textwrap import dedent
 
 # This represents the version of the rust-vmm-container used
 # for running the tests.
-CONTAINER_VERSION = "gd59baa3"
+CONTAINER_VERSION = "g260df6e"
 # The suffix suggests that the dev image with `v{N}-riscv` tag is not to be
 # confused with real `riscv64` image (it's actually a `x86_64` image with
 # `qemu-system-riscv64` installed), since AWS yet has `riscv64` machines
